03287841d8857532a2f8c57c1affe5b0a3a284d8
Thesam1798 <20329547+Thesam1798@users.noreply.github.com>
2018-12-21 01:06:47 +0100

    (Fix) Mission statement
    Adding all steps.
    Closing the web browser.
    Git deployment.

--- main.c
+++ main.c
@@ -2,5 +2,38 @@
 
 int main(void)
 {
-	printf("Hello world!");
+	//Starting Challenge
+	int step = 0;
+	float time = 0.50; //the decimal places are the minutes for the real hour 
+
+	printf("Ctrl+Maj+i");
+
+	//URL found (easy)
+	step = step + 1;
+	time = time + 0.02;
+
+	//01101000 ? 
+	printf("Bin to Ascii");
+
+	//binaryhexconverter.com
+	step = step + 1;
+	time = time + 0.03;
+
+	printf("Kevin Mitnick");
+
+	//Tsutomu Shimomura => Kevin Mitnick (luck?)
+	step = step + 1;
+	time = time + 0.07;
+	
+	printf("Automates");
+
+	//wget https://fr.wikipedia.org/wiki/Expression_r%C3%A9guli%C3%A8re | regex101
+	step = step + 1;
+	time = time + 0.02;
+
+
+	printf("Git");
+
+	//YES
+	return step;
 }
